61e50f0337cd041bbde99c6b9534b3734c27c64d
Saar Shen <xiaomi7732@hotmail.com>
2015-03-30 12:05:28 -0700

    Update assembly info. Get prepared for packing up to NuGet.

--- LiveSDK.ObjectModel.UniversalApps/Properties/AssemblyInfo.cs
+++ LiveSDK.ObjectModel.UniversalApps/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LiveSDK.ObjectModel.UniversalApps")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("LiveSDK Object Model")]
+[assembly: AssemblyDescription("LiveSDK Object Model for Windows Universal Apps")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LiveSDK.ObjectModel.UniversalApps")]
+[assembly: AssemblyCompany("Saar")]
+[assembly: AssemblyProduct("LiveSDKOM")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
