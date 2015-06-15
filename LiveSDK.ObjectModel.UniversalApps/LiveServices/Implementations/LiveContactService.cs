502aa42e18511e3a320bab8ca645c8a185d6cbf2
Saar Shen <xiaomi7732@hotmail.com>
2015-06-14 20:28:43 -0700

    Add overrides to get/download a contact picture by id.

--- LiveSDK.ObjectModel.UniversalApps/LiveServices/Implementations/LiveContactService.cs
+++ LiveSDK.ObjectModel.UniversalApps/LiveServices/Implementations/LiveContactService.cs
@@ -40,18 +40,28 @@ namespace LiveSDK.ObjectModel.LiveServices.Implementations
             }
         }
 
-        public async Task<Picture> GetPictureAsync(Contact contact, CancellationToken? cancel = null)
+        public async Task<Picture> GetPictureAsync(string contactId, CancellationToken? cancel = null)
         {
             LiveConnectClient connectClient = await GetConnectClientAsync();
-            Picture picture = await connectClient.GetAsync<Picture>(string.Format("{0}/picture", contact.UserId), cancel);
+            Picture picture = await connectClient.GetAsync<Picture>(string.Format("{0}/picture", contactId), cancel);
             return picture;
+        }
+
+        public async Task<Picture> GetPictureAsync(Contact contact, CancellationToken? cancel = null)
+        {
+            return await GetPictureAsync(contact.Id, cancel);
+        }
+
+        public async Task DownloadPictureAsync(string contactId, IStorageFile resultFile, CancellationToken? cancel = null)
+        {
+            Picture picture = await GetPictureAsync(contactId, cancel);
+            Uri source = new Uri(picture.Location);
+            await DownloadFileAsync(source, resultFile, cancel);
         }
 
         public async Task DownloadPictureAsync(Contact contact, IStorageFile resultFile, CancellationToken? cancel = null)
         {
-            Picture picture = await GetPictureAsync(contact, cancel);
-            Uri source = new Uri(picture.Location);
-            await DownloadFileAsync(source, resultFile, cancel);
+            await DownloadPictureAsync(contact.Id, resultFile, cancel);
         }
     }
 }
--- LiveSDK.ObjectModel.UniversalApps/LiveServices/Interfaces/ILiveContactService.cs
+++ LiveSDK.ObjectModel.UniversalApps/LiveServices/Interfaces/ILiveContactService.cs
@@ -5,9 +5,11 @@ namespace LiveSDK.ObjectModel.LiveServices.Interfaces
 {
     public interface ILiveContactService
     {
+        Task DownloadPictureAsync(string contactId, IStorageFile resultFile, CancellationToken? cancel = null);
         Task DownloadPictureAsync(Contact contact, IStorageFile resultFile, CancellationToken? cancel = null);
         Task<Contacts> GetContactsAsync();
         Task<Contacts> GetContactsAsync(int skip = 0, int take = 50, System.Threading.CancellationToken? cancel = null);
-        Task<Picture> GetPictureAsync(LiveSDK.ObjectModel.Contact contact, System.Threading.CancellationToken? cancel = null);
+        Task<Picture> GetPictureAsync(Contact contact, CancellationToken? cancel = null);
+        Task<Picture> GetPictureAsync(string contactId, CancellationToken? cancel = null);
     }
 }
